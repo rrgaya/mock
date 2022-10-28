4d685d715bf5e9d5c1a8647a07dc2dab2536b233
Gaya <rrgaya@gmail.com>
2022-10-28 09:17:49 -0300

    Refactor user_test added mock

--- api/user_test.go
+++ api/user_test.go
@@ -1,18 +1,41 @@
 package api
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func (mock *MockHTTP) Do(_ *http.Request) (*http.Response, error) {
+	return mock.reponse, mock.err
+}
+
+type MockHTTP struct {
+	reponse *http.Response
+	err     error
+}
 
 func TestGetRandomUser(t *testing.T) {
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 
 	w := httptest.NewRecorder()
 
-	GetRandomUser(w, r)
+	res := http.Response{
+		Body: ioutil.NopCloser(strings.NewReader("<<MOCK>>")),
+	}
+
+	mock := MockHTTP{
+		reponse: &res,
+	}
+
+	service := Service{
+		HTTPClient: &mock,
+	}
+
+	service.GetRandomUser(w, r)
 
 	got := w.Result()
 
